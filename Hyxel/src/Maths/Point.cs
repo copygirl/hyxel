d40965b1e0be456f0a23afa47234122fe3efdb0d
copygirl <copygirl@mcft.net>
2018-12-07 09:19:37 +0100

    Improve Point and Size with operators and such

--- Hyxel/src/Maths/Point.cs
+++ Hyxel/src/Maths/Point.cs
@@ -2,12 +2,41 @@ using System;
 
 namespace Hyxel.Maths
 {
-  public struct Point
+  public struct Point : IEquatable<Point>
   {
+    public static readonly Point Origin = new Point(0, 0);
+    
+    
     public int X, Y;
     
     public Point(int x, int y)
       { X = x; Y = y; }
+    
+    
+    public static Point operator -(Point p)
+      => new Point(-p.X, -p.Y);
+    
+    public static Point operator *(Point p, int factor)
+      => new Point(p.X * factor, p.Y * factor);
+    public static Point operator /(Point p, int factor)
+      => new Point(p.X / factor, p.Y / factor);
+    
+    public static Point operator +(Point left, Point right)
+      => new Point(left.X + right.X, left.Y + right.Y);
+    public static Point operator -(Point left, Point right)
+      => new Point(left.X - right.X, left.Y - right.Y);
+    
+    public static bool operator ==(Point left, Point right) =>  left.Equals(right);
+    public static bool operator !=(Point left, Point right) => !left.Equals(right);
+    
+    
+    public bool Equals(Point other)
+      => (X == other.X) && (Y == other.Y);
+    public override bool Equals(object obj)
+      => (obj is Point other) ? Equals(other) : false;
+    
+    public override int GetHashCode()
+      => Tuple.Create(X, Y).GetHashCode(); // TODO: Optimize this?
     
     public override string ToString()
       => $"Point(x={X},y={Y})";
--- Hyxel/src/Maths/Size.cs
+++ Hyxel/src/Maths/Size.cs
@@ -2,12 +2,36 @@ using System;
 
 namespace Hyxel.Maths
 {
-  public struct Size
+  public struct Size : IEquatable<Size>
   {
+    public static readonly Size Zero = new Size(0, 0);
+    
+    
     public int Width, Height;
     
     public Size(int width, int height)
       { Width = width; Height = height; }
+    
+    
+    public static Size operator *(Size s, int factor)
+      => new Size(s.Width * factor, s.Height * factor);
+    public static Size operator /(Size s, int factor)
+      => new Size(s.Width / factor, s.Height / factor);
+    
+    public static bool operator ==(Size left, Size right) =>  left.Equals(right);
+    public static bool operator !=(Size left, Size right) => !left.Equals(right);
+    
+    public static explicit operator Size(Point p) => new Size(p.X, p.Y);
+    public static explicit operator Point(Size s) => new Point(s.Width, s.Height);
+    
+    
+    public bool Equals(Size other)
+      => (Width == other.Width) && (Height == other.Height);
+    public override bool Equals(object obj)
+      => (obj is Size other) ? Equals(other) : false;
+    
+    public override int GetHashCode()
+      => Tuple.Create(Width, Height).GetHashCode(); // TODO: Optimize this?
     
     public override string ToString()
       => $"Size(width={Width},height={Height})";
