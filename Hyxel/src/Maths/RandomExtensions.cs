cdc30a7562aed8ad90429de1315c4ed072ef1743
copygirl <copygirl@mcft.net>
2018-12-16 10:23:53 +0100

    Add RandomExtensions.NextBool

--- Hyxel/src/Maths/RandomExtensions.cs
+++ Hyxel/src/Maths/RandomExtensions.cs
@@ -5,16 +5,19 @@ namespace Hyxel.Maths
   public static class RandomExtensions
   {
     public static int Next(this Random rnd, int min, int maxExclusive)
-      => min + rnd.Next(maxExclusive - min);
+      => (min + rnd.Next(maxExclusive - min));
     public static int NextIncl(this Random rnd, int min, int maxInclusive)
-      => min + rnd.Next(maxInclusive + 1 - min);
+      => (min + rnd.Next(maxInclusive + 1 - min));
     
     public static float NextFloat(this Random rnd)
       => (float)rnd.NextDouble();
     public static float Next(this Random rnd, float min, float max)
-      => min + rnd.NextFloat() * (max - min);
+      => (min + rnd.NextFloat() * (max - min));
     public static double Next(this Random rnd, double min, double max)
-      => min + rnd.NextDouble() * (max - min);
+      => (min + rnd.NextDouble() * (max - min));
+    
+    public static bool NextBool(this Random rnd, double chance)
+      => (rnd.NextDouble() < chance);
     
     public static T Choose<T>(this Random rnd, params T[] elements)
       => elements[rnd.Next(elements.Length)];
